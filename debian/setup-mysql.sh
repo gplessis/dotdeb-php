0d162762efc931ed1dcbec3b6037333cd2fb5245
Guillaume Plessis <gui@dotdeb.org>
2012-06-21 22:28:35 -0700

    PHP 5.4.4 - Squeeze
      * Sync with Debian
      * New upstream release

--- debian/setup-mysql.sh
+++ debian/setup-mysql.sh
@@ -21,8 +21,8 @@ fi
 
 socket=$datadir/mysql.sock
 # Commands:
-mysqladmin="mysqladmin --user root --port $port --host localhost --socket=$socket --no-beep"
-mysqld="/usr/sbin/mysqld --no-defaults --user=$user --bind-address=localhost --port=$port --socket=$socket --datadir=$datadir"
+mysqladmin="mysqladmin --no-defaults --user root --port $port --host 127.0.0.1 --socket=$socket --no-beep"
+mysqld="/usr/sbin/mysqld --no-defaults --user=$user --bind-address=127.0.0.1 --port=$port --socket=$socket --datadir=$datadir"
 
 # Main code #
 
@@ -36,7 +36,7 @@ mkdir -p $datadir
 chmod go-rx $datadir
 chown $user: $datadir
 
-mysql_install_db --user=$user --datadir=$datadir --rpm --force >> $datadir/bootstrap.log 2>&1
+mysql_install_db --no-defaults --user=$user --datadir=$datadir --rpm --force
 
 tmpf=$(mktemp)
 cat > "$tmpf" <<EOF
@@ -45,18 +45,18 @@ UPDATE user SET password=PASSWORD('') WHERE user='root';
 FLUSH PRIVILEGES;
 EOF
 
-$mysqld --bootstrap --skip-grant-tables < "$tmpf" >> $datadir/bootstrap.log 2>&1
+$mysqld --bootstrap --skip-grant-tables < "$tmpf"
 
 unlink "$tmpf"
 
 # Start the daemon
-$mysqld > $datadir/run.log 2>&1 &
+$mysqld &
 
 pid=$!
 
 # Wait for the server to be actually available
 c=0;
-while ! nc -z localhost $port; do
+while ! nc -z 127.0.0.1 $port; do
     c=$(($c+1));
     sleep 3;
     if [ $c -gt 20 ]; then
