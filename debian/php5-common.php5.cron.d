c556bac78e2cd5652a72199a4727e28f944ea7dd
Ondřej Surý <ondrej@sury.org>
2012-11-29 20:43:16 +0100

    Fix typo in path to session clean script in cron file (Closes: #694736)

--- debian/php5-common.php5.cron.d
+++ debian/php5-common.php5.cron.d
@@ -4,4 +4,4 @@
 #  files, or 24 minutes if not defined.  See /usr/lib/php5/maxlifetime
 
 # Look for and purge old sessions every 30 minutes
-09,39 *     * * *     root   [ -x /usr/lib/php5/maxlifetime ] && [ -x /usr/lib/php5/sessionclean ] && [ -d /var/lib/php5 ] && /usr/lib/php5/sessionsclean /var/lib/php5 $(/usr/lib/php5/maxlifetime)
+09,39 *     * * *     root   [ -x /usr/lib/php5/maxlifetime ] && [ -x /usr/lib/php5/sessionclean ] && [ -d /var/lib/php5 ] && /usr/lib/php5/sessionclean /var/lib/php5 $(/usr/lib/php5/maxlifetime)
