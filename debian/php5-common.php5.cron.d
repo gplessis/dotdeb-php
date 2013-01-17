1b547025ddae524c95592c473cbc1b7e9c83679b
Guillaume Plessis <gui@dotdeb.org>
2013-01-17 18:25:01 +0100

    Introduce new (hopefully slightly smarter) way of not deleting still used session files

--- debian/php5-common.php5.cron.d
+++ debian/php5-common.php5.cron.d
@@ -4,4 +4,4 @@
 #  files, or 24 minutes if not defined.  See /usr/lib/php5/maxlifetime
 
 # Look for and purge old sessions every 30 minutes
-09,39 *     * * *     root   [ -x /usr/lib/php5/maxlifetime ] && [ -d /var/lib/php5 ] && find /var/lib/php5/ -depth -mindepth 1 -maxdepth 1 -type f -ignore_readdir_race -cmin +$(/usr/lib/php5/maxlifetime) ! -execdir fuser -s {} 2>/dev/null \; -delete
+09,39 *     * * *     root   [ -x /usr/lib/php5/maxlifetime ] && [ -x /usr/lib/php5/sessionclean ] && [ -d /var/lib/php5 ] && /usr/lib/php5/sessionsclean /var/lib/php5 $(/usr/lib/php5/maxlifetime)
