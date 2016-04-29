e4a0a86d4d8c29bf1208e6cc25484c988619a774
Guillaume Plessis <gui@dotdeb.org>
2016-04-29 12:33:02 +0000

    Imported Upstream version 5.6.21

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 20
+#define PHP_RELEASE_VERSION 21
 #define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.6.20"
-#define PHP_VERSION_ID 50620
+#define PHP_VERSION "5.6.21"
+#define PHP_VERSION_ID 50621
